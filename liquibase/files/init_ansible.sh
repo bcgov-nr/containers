d4247024f8672983e3b78afee8795c852cdf0a50
bferguso <brett@qedsystems.ca>
2021-12-22 13:21:46 -0800

    Feature/lb point to bcgov repo (#19)
    * Point the init_ansible script at the bcgov repo
    
    * Added newline to end of init_ansible.sh file

--- liquibase/files/init_ansible.sh
+++ liquibase/files/init_ansible.sh
@@ -1,2 +1,2 @@
-ansible-galaxy collection install git+https://www.github.com/bferguso/nr-ansible,feature/liquibase -p ./collections
+ansible-galaxy collection install git+https://www.github.com/bcgov/nr-ansible,main -p ./collections
 export ANSIBLE_COLLECTIONS_PATH=./collections
