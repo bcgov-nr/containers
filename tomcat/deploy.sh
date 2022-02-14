4ccef9a7c2e6e3913a50def3d364575d03c1da2b
ahbarnz-g <87829400+ahbarnz-g@users.noreply.github.com>
2022-02-14 09:41:06 -0800

    Feat/tomcat (#21)
    * feat: auto-start/systemd
    
    * feat: tomcat
    
    * copy Java JRE, tomcat based on args
    
    * tomcat works, but the mount points aren't

--- tomcat/deploy.sh
+++ tomcat/deploy.sh
@@ -9,12 +9,12 @@
 #%   ${THIS_FILE} [Options]
 #%
 #% Options:
-#%   -v, --version <version>    Version number of Tomcat to install, latest.  Default is ?.
-#%   -v, --version <version>    Version number of OpenJava to install, latest.  Default is ?.
-#%   -g, --generate             Generate the systemd unit file for the container. Default is false.
-#%   --env=/path/to/file        Specify path to env file. Default is ./local.env.
-#%   --rhel=7                   Specify the version of RHEL the container uses.  Default is RHEL8.
-#%   -h, -?, --help             Displays this help dialog.
+#%   -v, --version <version>        Version number of Tomcat to install, latest.  Default is 8.0.51.
+#%   -jv, --jreversion <version>    Version number of Java/JRE to install, latest.  Default is 8u192.
+#%   -p, --port <number>            Host port that tomcat (listening on 8443) can be reached at.  Default is 8123.
+#%   -g, --generate                 Generate the systemd unit file for the container. Default is false.
+#%   --rhel=7                       Specify the version of RHEL the container uses.  Default is RHEL8.
+#%   -h, -?, --help                 Displays this help dialog.
 #%
 
 # Specify halt conditions (errors, unsets, non-zero pipes) and verbosity
@@ -26,10 +26,11 @@ die() {
     exit 1; 
 }
 
+CONTAINER_NAME="tomcat"
 TOMCAT_VERSION="8.0.51"
-JRE_VERSION="1.8.0"
-LOG4J_VERSION=""
-ENVFILE="$(pwd)/local.env"
+JRE_VERSION="8u192"
+HOST_PORT="8123"
+# ENVFILE="$(pwd)/local.env"
 TOMCAT_LABEL_ENV="local"
 RHEL_VERSION_MIN=7
 RHEL_VERSION_MAX=8
@@ -53,14 +54,14 @@ do
         -g|--generate)
             ISGENERATE=true
             ;;
-        --env=?*) 
-            # Delete everything up to "=" and assign the remainder:
-            ENVFILE=${1#*=}
-            ;;
-        --env=) 
-            # Handle the case of an empty --env=
-            die 'ERROR: "--env" requires a non-empty option argument.'
-            ;;    
+        # --env=?*) 
+        #     # Delete everything up to "=" and assign the remainder:
+        #     ENVFILE=${1#*=}
+        #     ;;
+        # --env=) 
+        #     # Handle the case of an empty --env=
+        #     die 'ERROR: "--env" requires a non-empty option argument.'
+        #     ;;    
         --rhel=?*) 
             # Delete everything up to "=" and assign the remainder:
             RHEL_VERSION=${1#*=}
@@ -99,13 +100,16 @@ do
             else
                 die 'ERROR: "--jreversion" requires a non-empty option argument.'
             fi
-            ;;
-        -lv|--log4jversion) 
+            ;;  
+        -p|--port) 
             if [ "$2" ] 
             then
-                LOG4J_VERSION=$2
+                HOST_PORT=$2
+                shift
+            else
+                die 'ERROR: "--jreversion" requires a non-empty option argument.'
             fi
-            ;;                        
+            ;;                                  
         --)  # End of all options.
             shift
             break
@@ -129,16 +133,61 @@ fi
 # Set image and build, if necessary
 if [ "${TOMCAT_LABEL_ENV}" == "local" ]
 then
-    podman build -t tomcat:"${TOMCAT_VERSION}-j${OPENJDK_VERSION}-rhel${RHEL_VERSION}" --build-arg TOMCATVERSION="${TOMCAT_VERSION}" ${DOCKERFILE_LOCATION}
-    IMAGE="localhost/tomcat:${TOMCAT_VERSION}-j${OPENJDK_VERSION}-rhel${RHEL_VERSION}"
+    podman build -t "${CONTAINER_NAME}":"${TOMCAT_VERSION}-jre${JRE_VERSION}" --build-arg TOMCATVERSION="${TOMCAT_VERSION}" --build-arg JREVERSION="${JRE_VERSION}" ${DOCKERFILE_LOCATION}
+    IMAGE="localhost/${CONTAINER_NAME}:${TOMCAT_VERSION}-jre${JRE_VERSION}"
+
+    if [ ! -d conf ]; then
+      mkdir -p conf;
+    fi
+
+    if [ ! -d logs ]; then
+      mkdir -p logs;
+    fi
+
+    if [ ! -d webapps ]; then
+      mkdir -p webapps;
+    fi   
 else
-    IMAGE="ghcr.io/bcgov/nr-ansible-tomcat:${TOMCAT_VERSION}-rhel${RHEL_VERSION}"
+    IMAGE="ghcr.io/bcgov/nr-ansible-${CONTAINER_NAME}:${TOMCAT_VERSION}-jre${JRE_VERSION}"
 fi
 
 # log-opt path - use absolute path ONLY; do not point to Windows drives on WSL 
-podman run -i -t --rm --name tomcat
+podman run -i -t --rm --name "${CONTAINER_NAME}" \
+    -v "$(pwd)/logs:/usr/local/tomcat/logs:z" \
+    -v "$(pwd)/webapps:/usr/local/tomcat/webapps:z" \
+    -p ${HOST_PORT}:8443/tcp \
+    "${IMAGE}"
 
-if "${ISGENERATE}"
-then
-    podman generate systemd --new --files --name tomcat:"${TOMCAT_VERSION}"
-if
+# podman run -i -t --rm --name "${CONTAINER_NAME}" \
+#     # --pid="host" \
+#     # -v "$(pwd)/conf:/usr/local/tomcat/conf:z" \
+#     # -v "$(pwd)/logs:/usr/local/tomcat/logs:z" \
+#     # -v "$(pwd)/webapps:/usr/local/tomcat/webapps:z" \
+#     # -v "/proc/stat:/proc/stat:z" \
+#     -p ${HOST_PORT}:8443/tcp \
+#     "${IMAGE}"
+
+# if "${ISGENERATE}"
+# then
+#     podman generate systemd --new --files --name "${CONTAINER_NAME}"
+
+#     ln -s container-"${CONTAINER_NAME}".service /etc/systemd/system
+
+#     if ( (which systemctl))
+#     then
+
+#         systemctl --user enable container-"${CONTAINER_NAME}".service
+
+#         # Confirm systemd config
+#         systemctl --user is-enabled container-"${CONTAINER_NAME}".service
+
+#         # Start the service
+#         systemctl --user start container-"${CONTAINER_NAME}".service
+
+#         # Confirm the status of the service
+#         systemctl --user status container-"${CONTAINER_NAME}".service
+#     else
+#         echo -e "\nPlease verify systemctl is installed\n"
+#         exit
+#     fi
+# if
